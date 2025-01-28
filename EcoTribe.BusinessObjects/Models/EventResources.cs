e851cc60da412739b54ef5d4d3303aa9002bfb44
Nikolay Uzunov <nikiuzunovv1122@gmail.com>
2025-01-28 14:41:01 +0200

    add models and updated project file

---
+++ EcoTribe.BusinessObjects/Models/EventResources.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoTribe.BusinessObjects.Models
+{
+    internal class EventResources
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        public Event Event { get; set; } = null!;
+        public string ResourceName { get; set; } = null!;
+        public int Quantity { get; set; }
+        public string? ProvidedBy { get; set; }
+        public string Status { get; set; }
+        public string? Notes { get; set; }
+    }
+}
---
+++ EcoTribe.BusinessObjects/Models/Volunteer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoTribe.BusinessObjects.Models
+{
+    internal class Volunteer
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Skills { get; set; }
+        public string? PreferredEvents { get; set; }
+        public decimal Latitude { get; set; } 
+        public decimal Longitude { get; set; } 
+        public string Number { get; set; } = null!;
+        public string? Instagram { get; set; }
+        public string? Facebook { get; set; }
+    }
+}
