0c8c556b854c2e255e91690de7a55d6399a7777b
Nikolay Uzunov <nikiuzunovv1122@gmail.com>
2025-03-16 17:21:34 +0200

    Add new role organizator via identity.

--- EcoTribe.Data/DataSeeder.cs
+++ EcoTribe.Data/DataSeeder.cs
@@ -16,7 +16,7 @@ namespace EcoTribe.Data
             var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
             var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
-            string[] roleNames = { "Administrator", "User" };
+            string[] roleNames = { "Administrator", "User", "Organizator" };
 
             foreach (var role in roleNames)
             {
@@ -32,11 +32,39 @@ namespace EcoTribe.Data
             var adminUser = await userManager.FindByEmailAsync(adminEmail);
             if (adminUser == null)
             {
-                var newAdmin = new ApplicationUser { UserName = adminEmail, Email = adminEmail, EmailConfirmed = true, FirstName = "Admin" };
+                var newAdmin = new ApplicationUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    EmailConfirmed = true,
+                    FirstName = "Admin" 
+                };
+
                 var result = await userManager.CreateAsync(newAdmin, adminPassword);
                 if (result.Succeeded)
                 {
                     await userManager.AddToRoleAsync(newAdmin, "Administrator");
+                }
+            }
+
+            string organizatorEmail = "organizator@example.com";
+            string organizatorPassoword = "Organizator123!";
+
+            var organizatorUser = await userManager.FindByEmailAsync(organizatorEmail);
+            if (organizatorUser == null)
+            {
+                var newOrganizator = new ApplicationUser
+                {
+                    UserName = organizatorEmail,
+                    Email = organizatorEmail,
+                    EmailConfirmed = true,
+                    FirstName = "Organizator"
+                };
+
+                var organizatorResult = await userManager.CreateAsync(newOrganizator, organizatorPassoword);
+                if (organizatorResult.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(newOrganizator, "Organizator");
                 }
             }
         }
